d9d0532ee5366f33ac6a7470c0546949020f716f
andrew goodman <computedscientist@gmail.com>
2024-12-03 16:06:59 -0500

    1_add-introductory-java-programming-examples: add unit test for Variables default constructor

---
+++ foundations/variables/src/test/java/org/mga/compsciknights/java/foundations/VariablesTest.java
@@ -0,0 +1,35 @@
+package org.mga.compsciknights.java.foundations;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+class VariablesTest {
+
+    org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(VariablesTest.class);
+
+    @Test
+    void defaultConstructorSetsAllMemberVariable() {
+        log.info("START verifying the Variables default constructor");
+        Variables variables = new Variables();
+
+        assertThat(
+                "Default value for aString", variables.getaString(),
+                equalTo("this is a string.")
+        );
+        assertThat("Default value for anInt", variables.getAnInt(),
+            equalTo(0)
+        );
+        assertThat("Default value for aFloat", variables.getaFloat(),
+            equalTo(0F)
+        );
+        assertThat("Default value for aDouble", variables.getaDouble(),
+            equalTo(0D)
+        );
+        log.info("DONE verifying the Variables default constructor");
+    }
+
+}
