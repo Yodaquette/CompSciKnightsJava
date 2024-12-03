5e10b974c720b7c4b80f3f69aaf5c08ee7a3ce30
andrew goodman <computedscientist@gmail.com>
2024-12-02 23:25:42 -0500

    1_add-introductory-java-programming-examples: add hello world program example

---
+++ foundations/hello-world/src/main/java/org/mga/compsciknights/java/foundations/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.mga.compsciknights.java.foundations;
+
+public class HelloWorld {
+
+    /**
+     * The obligatory programming example that accomplishes very little
+     * other than printing some text to the screen
+     *
+     * @param args An array of arbitrary length that can be used to pass
+     *             values into the main method
+     * @throws Exception A generic exception class that can be extended to
+     *                   customize handling specific error types
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+}
