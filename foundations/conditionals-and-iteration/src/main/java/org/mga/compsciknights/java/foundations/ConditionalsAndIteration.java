be753391cc249afccd2d2f8dbcd3494b5a107b24
andrew goodman <computedscientist@gmail.com>
2024-12-08 03:32:12 -0500

    1_add-introductory-java-programming-examples: add example for contitions and iterations

---
+++ foundations/conditionals-and-iteration/src/main/java/org/mga/compsciknights/java/foundations/ConditionalsAndIteration.java
@@ -0,0 +1,29 @@
+package org.mga.compsciknights.java.foundations;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConditionalsAndIteration {
+
+    private static final Logger log = LoggerFactory.getLogger(ConditionalsAndIteration.class);
+
+    public static void main(String[] args) throws Exception {
+        String message;
+        if (args == null || args.length == 0) {
+            message = "Program input is empty.";
+            log.info(message);
+            return;
+        } else if (args.length == 1) {
+            message = String.format("Program has %d input value.", args.length);
+        } else {
+            message = String.format("Program has %d input values.", args.length);
+        }
+        log.info(message);
+
+        log.info("Printing program input now...");
+        for (String arg : args) {
+            log.info(arg);
+        }
+    }
+
+}
